648374a6492c7415febdca7c12699bc55269de3a
Dasiel Otero Dartayet <dasiel@gmail.com>
2020-02-19 14:51:41 -0500

    ADD pagina de registro, dependencia de axios para crear los servicios, y la ruta de registro

--- client/client/src/components/layout.js
+++ client/client/src/components/layout.js
@@ -9,6 +9,7 @@ import {
     Redirect
 } from 'react-router-dom';
 import Profile from './profile';
+import Registry from './registry/registry';
 
 class Layout extends React.Component {
     render() {
@@ -22,6 +23,9 @@ class Layout extends React.Component {
                 </Route>
                 <Route path="/profile">
                   <Profile />
+                </Route>
+                <Route path="/registry">
+                  <Registry />
                 </Route>
               </Switch>
             </Router>
