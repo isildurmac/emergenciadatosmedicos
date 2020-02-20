2cc5c9e289a0c3e3d9b6af9673e88bfd3a3119b9
GYPR <giselle.designe@gmail.com>
2020-02-20 19:22:02 +0100

    Adding validations to user entity

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,23 +1,24 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, Timestamp, BeforeInsert, ManyToOne } from 'typeorm';
+import { IsString, IsNumber, IsOptional, IsDate } from 'class-validator';
 import { Role } from './role';
 import { hashSync } from 'bcrypt';
 
 @Entity('users')
 export class User {
   @PrimaryGeneratedColumn()
-  id: number;
+  @IsNumber() @IsOptional() readonly id: number;
   @Column({ name: 'name', length: 191 })
-  name: string;
+  @IsString() readonly name: string;
   @Column({ name: 'password', length: 191 })
-  password: string;
+  @IsString() readonly password: string;
   @Column({ name: 'email', length: 191 })
-  email: string;
+  @IsString() readonly email: string;
   @Column({ name: 'ci', length: 11 })
-  ci: string;
+  @IsString() readonly ci: string;
   @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
-  createdAt: Timestamp;
+  @IsDate() readonly createdAt: Timestamp;
   @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
-  updatedAt: Timestamp;
+  @IsDate() readonly updatedAt: Timestamp;
 
   @ManyToOne(type => Role, role => role.users )
   role: Role;
