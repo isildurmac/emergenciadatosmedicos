ef0d560e4d3c5b7005e917efd22707ca1d81ee4f
Dasiel Otero Dartayet <dasiel@gmail.com>
2020-02-20 14:33:27 -0500

    MOD CRUD user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param,Post,Body,Put,Delete } from '@nestjs/common';
 import { UserRepository } from 'src/repositories/user-repository';
+import { UserDTO } from 'src/dtos/user.dto';
 
 @Controller('user')
 export class UserController {
@@ -9,5 +10,23 @@ export class UserController {
   async findAll(): Promise<any[]> {
     return this.userRepo.find();
   }
-  
+  @Get(':id')
+  findOne(@Param() params): Promise<any> {
+    console.log(params.id);
+    return this.userRepo.findOne(params.id);
+  }
+  @Post()
+  create(@Body() userDTO: UserDTO) {
+    return this.userRepo.save(userDTO);
+  }
+  @Put(':id')  
+  update(@Param('id') id: string, @Body() userDTO: UserDTO) {
+    return this.userRepo.update(id,userDTO);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userRepo.delete(id);
+  }
+
 }
