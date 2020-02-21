52684b3e835873d3b045d80d95028f1c5da312d5
Dasiel Otero Dartayet <dasiel@gmail.com>
2020-02-21 15:41:04 -0500

    MOD Se inserta el usuario enla base de datos

--- client/client/src/components/registry/registry.js
+++ client/client/src/components/registry/registry.js
@@ -1,102 +1,117 @@
 import React from 'react';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
-import {Dropdown} from 'primereact/dropdown';
-import  RegistryService  from '../../services/RegistryService';
+import { Dropdown } from 'primereact/dropdown';
+import RegistryService from '../../services/RegistryService';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 
 
 const sexoSelectItems = [
-  {label: 'Masculino', value: 'M'},
-  {label: 'Femenino', value: 'F'}
- ];
+  { label: 'Masculino', value: 'M' },
+  { label: 'Femenino', value: 'F' }
+];
 
 class Registry extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      users: []
+      users: [],
+      data:{
+      name:'',
+      email:'',
+      ci:'',
+      userName:'',
+      password:'', 
+      contrasena1:'',
+      address:'',
+      gender:'' }
     };
-
     this.registryService = new RegistryService();
   }
+onChange = e => 
+    this.setState({
+      data:{...this.state.data,[e.target.name]:e.target.value}
+    });
 
-  
   componentDidMount() {
     this.registryService.obtenerResultado().then(res => {
       const users = res.data;
-      console.log(users);
       this.setState({ users });
+     })
+  }
+
+  onSubmit=()=>{
+    this.registryService.saveUser(this.state.data).then(res=>{
+      console.log (res);
+      this.preventDefault();
     })
   }
   render() {
     return (
-      
-    
       <section className="section_padding">
         <div className="container">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <span className="p-float-label">
-              <InputText id="nombre" />
+              <InputText id="nombre" name='name' value={this.state.data.name} onChange={this.onChange}/>
               <label htmlFor="in">Nombre</label>
             </span>
             <br />
             <span className="p-float-label">
-              <InputText id="ci" />
+              <InputText id="ci" name='ci' value={this.state.data.ci} onChange={this.onChange}/>
               <label htmlFor="in">CI</label>
             </span>
             <br />
             <span className="p-float-label">
-              <InputText id="direccion" />
+              <InputText id="direccion" name='address' value={this.state.data.address} onChange={this.onChange}/>
               <label htmlFor="in">Direcci&oacute;n</label>
             </span>
             <br />
             <span className="p-float-label">
-              <InputText type="email" id="correo" />
+              <InputText type="email" id="correo" name='email' value={this.state.data.email} onChange={this.onChange} />
               <label htmlFor="in">Correo</label>
             </span>
             <br />
             <span className="p-float-label">
-              <InputText id="usuario" />
+              <InputText id="usuario" name='userName' value={this.state.data.userName} onChange={this.onChange} />
               <label htmlFor="in">Usuario</label>
             </span>
             <br />
-            <Dropdown value={this.state.sexo} options={sexoSelectItems} onChange={(e) => {this.setState({sexo: e.value})}} placeholder="Seleccione el sexo"/>
+            <Dropdown value={this.state.data.gender} name='gender' options={sexoSelectItems} onChange={this.onChange} placeholder="Seleccione el sexo" />
             <br />
             <br />
             <span className="p-float-label">
-              <InputText type="password" id="contrasena" />
+              <InputText type="password" id="contrasena" name='password' value={this.state.data.password} onChange={this.onChange}/>
               <label htmlFor="in">Contraseña</label>
             </span>
             <br />
             <Button label="Aceptar" />
           </form>
-         <br/>
-         <DataTable value={this.state.users}>
-                  <Column field="name" header="Nombre" style={{ width: 'auto' }} />
-                  <Column
-                    field="email"
-                    header="Email"
-                    style={{ width: 'auto' }}
-                  />
-                 <Column
-                    field="ci"
-                    header="CI"
-                    style={{ width: 'auto' }}
-                  />
-                  <Column
-                    field="address"
-                    header="Dirección"
-                    style={{ width: 'auto' }}
-                  />
-                  <Column
-                    field="gender"
-                    header="Sexo"
-                    style={{ width: '10%' }}
-                  />
-                </DataTable>
+          <br />
+          <DataTable value={this.state.users}>
+            <Column field="name" header="Nombre" style={{ width: 'auto' }} />
+            <Column
+              field="email"
+              header="Email"
+              style={{ width: 'auto' }}
+            />
+            <Column
+              field="ci"
+              header="CI"
+              style={{ width: 'auto' }}
+            />
+            <Column
+              field="address"
+              header="Dirección"
+              style={{ width: 'auto' }}
+            />
+            <Column
+              field="gender"
+              header="Sexo"
+              style={{ width: '10%' }}
+            />
+          </DataTable>
         </div>
       </section>
 
--- client/client/src/services/RegistryService.js
+++ client/client/src/services/RegistryService.js
@@ -3,4 +3,7 @@ export default class RegistryService {
     obtenerResultado = async () => {
         return axios.get('http://localhost:3000/user');
     }
+    saveUser = async (userDTO) => {
+        return axios.post('http://localhost:3000/user',userDTO);
+    }
 }
