7a8ca50de76c919822e19cc4beee88c4e2900ab0
ysoto86 <ysoto@soaint.com>
2020-02-21 10:19:24 -0500

    add funcionalidades para ecriptar y desencriptar

--- client/client/src/tools/helpers.js
+++ client/client/src/tools/helpers.js
@@ -7,8 +7,19 @@ export const helpers = {
 helpers.generateQR = async (text) => {
     try {
     return await QRCode.toDataURL(text)
-    // console.log('la url', url);
     } catch (err) {
       console.error(err)
     }
   };
+
+  
+helpers.encryptText = async (text) =>{
+
+  return Buffer.from(text).toString('base64');
+  }
+  
+  helpers.decryptText = async (text) =>{
+  
+  return Buffer.from(text,'base64').toString('ascii');
+    }
+  
